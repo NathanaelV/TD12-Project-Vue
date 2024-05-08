d7b5d5f442c948162592c8246417cba48d4df5e7
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-08 17:21:55 -0300

    Using Mustache and v-bind

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp ({
-
+    data(){
+        return{
+            firstName: 'Casey',
+            lastName: 'Jones',
+            email: 'casey.jones@email.com',
+            city: 'New York City',
+            picture: 'https://randomuser.me/api/portraits/men/57.jpg'
+        }
+    }
 })
 
 app.mount('#app');
